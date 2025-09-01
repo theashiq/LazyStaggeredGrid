0a75fa5663de259e93797a398a1c936c597dd69e
Ashiqur Rahman <ashiqur.rahman@hotmail.com>
2025-09-01 16:39:13 +0600

    Initial commit

---
+++ LazyStaggeredGrid/LazyStaggeredGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyStaggeredGridApp.swift
+//  LazyStaggeredGrid
+//
+//  Created by Ashiqur Rahman on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LazyStaggeredGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
